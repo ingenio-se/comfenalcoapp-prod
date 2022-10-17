3ee0e8809234361cff9cb708df6dba5579709ad7
David Guerra <davidguerra@Davids-iMac.local>
2022-10-17 10:31:10 -0500

    20221017 decure asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,25 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    @if (env('APP_ENV')!='Production')
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    @else
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    @endif
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+   
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <!--<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">-->
    
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @if (env('APP_ENV')!='Production')
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @else
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    @endif
+  
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
 </head>
 <body>
